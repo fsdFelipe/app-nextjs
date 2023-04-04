ad8cd4ddf8b5a62d95093d600ca40bc77a82e135
fsdFelipe <devfsdfelipe@gmail.com>
2023-04-04 13:03:23 -0300

    Adicionando funcionalidade para menu mobile Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,14 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState }from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+      };
+
   return (
     <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
         <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
@@ -25,11 +31,19 @@ const Navbar = () => {
         </ul>
 
         {/** Mobile Button */}
-        <div className='block sm:hidden z-10'>
+        <div onClick={handleNav} className='block sm:hidden z-10'>
+        {nav ? (
+            <AiOutlineClose size={20}  />
+          ) : (
             <AiOutlineMenu size={20} />
+          )}
         </div>
         {/** Mobile Menu */}
-        <div className='sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'>
+        <div className={
+            nav
+              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          }>
         <ul>
             <li className='p-4 text-4xl hover:text-gray-500'>
                 <Link href='/'>Home</Link>
