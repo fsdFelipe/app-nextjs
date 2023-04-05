9369d649b34e64ad127a0379adbd9a0a4b2ea14c
fsdFelipe <devfsdfelipe@gmail.com>
2023-04-05 18:33:26 -0300

    Finalizando Gallery

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { SliderData } from './SliderData';
 import Image from 'next/image'
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 const Slider = ({slides}) => {
     const [current, setCurrent] = useState(0);
@@ -18,9 +19,9 @@ const Slider = ({slides}) => {
       }
 
   return (
-    <div id='gallery'>
-        <h1>Gallery</h1>
-        <div>
+    <div id='gallery' className='max-w-[1240px] mx-auto'>
+        <h1 className='text-2xl font-bold text-center p-4'>Gallery</h1>
+        <div className='relative flex justify-center p-4'>
         {SliderData.map((slide, index) => {
         return(
             <div key={index} className={
@@ -28,6 +29,11 @@ const Slider = ({slides}) => {
                   ? 'opacity-[1] ease-in duration-1000'
                   : 'opacity-0'
               }>
+            <FaArrowCircleLeft
+                onClick={prevSlide}
+                className='absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]'
+                size={50}
+              />
             {index === current && (
                 <Image
                   src={slide.image}
@@ -37,6 +43,11 @@ const Slider = ({slides}) => {
                   object-fit='cover'
                 />
               )}
+              <FaArrowCircleRight
+                onClick={nextSlide}
+                className='absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]'
+                size={50}
+              />
             </div>
         ) 
     })}
