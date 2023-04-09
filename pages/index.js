6abd34af01a8c7e979c488e895ff12d13fdd537f
fsdFelipe <devfsdfelipe@gmail.com>
2023-04-09 11:08:41 -0300

    Adicionado Componente Instagram e InstagramImg

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Hero } from '@/components/Hero'
 import  Slider  from '@/components/Slider'
 import { SliderData } from '@/components/SliderData'
+import Instagram from '@/components/Instagram'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function Home() {
       </Head>
       <Hero heading='Captur Photography' message='I capture moments in nature and keep them alive.'/>
       <Slider slides={SliderData}/>
+      <Instagram />
     </>
   )
 }
