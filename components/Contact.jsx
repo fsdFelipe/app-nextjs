dd47372fd70841bf985325c2b1067e756366cca6
fsdFelipe <devfsdfelipe@gmail.com>
2023-04-09 12:37:42 -0300

    Finalizado Componente Contato

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        <h1>Let's work together</h1>
-        <form >
-            <div>
-                <input type="text" placeholder='Name' />
-                <input type="email" placeholder='Email' />
+    <div className='max-w-[1240px] m-auto p-4 h-screen'>
+        <h1 className='text-2xl font-bold text-center p-4'>Let's work together</h1>
+        <form className='max-w-[600px] m-auto'>
+            <div className='grid grid-cols-2 gap-2'>
+                <input className='border shadow-lg p-3' type="text" placeholder='Name' />
+                <input className='border shadow-lg p-3' type="email" placeholder='Email' />
             </div>
-            <input type="text" placeholder='Subject' />
-            <textarea cols="30" rows="10" placeholder='Message'></textarea>
-            <button>Submit</button>
+            <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
+            <textarea className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
+            <button className='border shadow-lg p-3 w-full mt-2'>Submit</button>
         </form>
     </div>
   )
